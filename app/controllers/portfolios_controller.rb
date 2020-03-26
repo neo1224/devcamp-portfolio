bb7d03366944d595d7b546522d98b9c089dd7faf
noel ballarta <nnballarta@up.edu.ph>
2020-03-26 11:21:21 +0800

    Implemented edit and update functionality

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,4 +1,5 @@
 class PortfoliosController < ApplicationController
+ #before_action :set_portfolio, only: [:show, :edit, :update, :destroy]
 	def index 
 		@portfolio_items = Portfolio.all
 	end
@@ -6,11 +7,8 @@ class PortfoliosController < ApplicationController
 	def new
 		@portfolio_item = Portfolio.new
 	end
-
-	def new2
-		@portfolio_item = Portfolio.new
-	end
-
+	
+	
 	def create
     @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
 
@@ -22,4 +20,29 @@ class PortfoliosController < ApplicationController
       end
      end
     end
+
+    def edit
+    	@portfolio_item = Portfolio.find(params[:id])
+    end
+
+    def update
+    	@portfolio_item = Portfolio.find(params[:id])
+
+    respond_to do |format|
+      if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
+        format.html { redirect_to portfolios_path, notice: 'Portfolio record was successfully updated.' }
+        else
+        format.html { render :edit }
+      end
+    end
+  end
+
+    # def set_portfolio
+    #   @portfolio_item = Portfolio.find(params[:id])
+    # end
+
+    # def portfolio_params
+    #   params.require(:blog).permit(:title, :body)
+    # end
+
 end
