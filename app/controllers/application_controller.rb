c158a9887c6205ce9aa91f794bd34da274c2af7d
noel ballarta <nnballarta@up.edu.ph>
2020-06-12 11:56:13 +0800

    added custom layouts for blogs and portfolios

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,8 @@
 class ApplicationController < ActionController::Base
-include DeviseWhitelist
-include SetSource
-include CurrentUserConcern
+	include DeviseWhitelist
+	include SetSource
+	include CurrentUserConcern
+	include DefaultPageContent
+
 end
+
